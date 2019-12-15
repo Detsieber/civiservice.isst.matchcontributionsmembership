e4d75915835c10776508639fc5ae7dc35aa61e33
p503021 <p503021@localdomain>
2019-12-15 23:03:41 +0100

    Module is functional

--- api/v3/Job/Matchcontributionsmemberships.php
+++ api/v3/Job/Matchcontributionsmemberships.php
@@ -16,31 +16,45 @@ function civicrm_api3_job_Matchcontributionsmemberships($params) {
     $opencontribution = civicrm_api3('Contribution', 'get', [
       'sequential' => 1,
       'return' => ["id", "contact_id", "receive_date", "total_amount"],
-      'financial_type_id' => "Membership Fee",
+      'financial_type_id' => 2,
       'contribution_status_id' => "Completed",
-      'options' => ['sort' => "receive_date DESC", 'limit' => 10],
+      'options' => ['sort' => "receive_date ASC", 'limit' => 10000],
     ]);
+
+
 
 // Now search for an entry in civicrm_membership_payment
   foreach ($opencontribution["values"] as $opencontributions) {
     $membership_payment = civicrm_api3('MembershipPayment', 'get', [
       'sequential' => 1,
       'return' => ["membership_id", "id"],
+      'contribution_id' => $opencontributions[id],
     ]);
+
 // entry exists already
     if ($membership_payment[count] > 0) {
     }
+
+
 // entry doesn't exist yet: search for a membership
     else {
-      $membership = civicrm_api3('Membership', 'getsingle', [
+      $membership = civicrm_api3('Membership', 'get', [
         'return' => ["id", "end_date", "status_id"],
-        'contact_id' => $opencontribution[contact_id],
+        'sequential' => 1,
+        'contact_id' => $opencontributions[contact_id],
+        'options' => ['sort' => "id DESC", 'limit' => 1],
       ]);
+
+// skip contributions without any membership entry
+// todo: create membership in these cases
+      if ($membership[count] > 0) {
+   
 
       $result = civicrm_api3('MembershipPayment', 'create', [
         'membership_id' => $membership[id],
-        'contribution_id' => $opencontribution[id],
+        'contribution_id' => $opencontributions[id],
       ]);
+      }
     }
   }
 }
