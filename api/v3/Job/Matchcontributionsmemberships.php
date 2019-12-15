6933cb8f55bc6d8226986ab55750a0063df4b122
Detlev Sieber <detlev.sieber@t-online.de>
2019-12-15 20:15:53 +0100

    Update Matchcontributionsmemberships.php

--- api/v3/Job/Matchcontributionsmemberships.php
+++ api/v3/Job/Matchcontributionsmemberships.php
@@ -56,29 +56,3 @@ foreach ($opencontribution["values"] as $opencontributions) {
     }
 }
 
-
-  
-  
-  
-   
- /* 
-  
-  if (array_key_exists('magicword', $params) && $params['magicword'] == 'sesame') {
-    $returnValues = array(
-      // OK, return several data rows
-      12 => array('id' => 12, 'name' => 'Twelve'),
-      34 => array('id' => 34, 'name' => 'Thirty four'),
-      56 => array('id' => 56, 'name' => 'Fifty six'),
-    );
-    // ALTERNATIVE: $returnValues = array(); // OK, success
-    // ALTERNATIVE: $returnValues = array("Some value"); // OK, return a single value
-
-    // Spec: civicrm_api3_create_success($values = 1, $params = array(), $entity = NULL, $action = NULL)
-    return civicrm_api3_create_success($returnValues, $params, 'NewEntity', 'NewAction');
-  }
-  else {
-    throw new API_Exception(/*errorMessage*/ 'Everyone knows that the magicword is "sesame"', /*errorCode*/ 1234);
-  }
-}
-*/
-  
