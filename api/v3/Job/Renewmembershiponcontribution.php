4115f687ebcd54c7d087606d2dde5a69cdc2b459
p503021 <p503021@localdomain>
2019-12-16 13:35:40 +0100

    div. updates

---
+++ api/v3/Job/Renewmembershiponcontribution.php
@@ -0,0 +1,18 @@
+<?php
+use CRM_Matchcontributionsmemberships_ExtensionUtil as E;
+
+/**
+ * Job.Renewmembershiponcontribution API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+
+function civicrm_api3_job_Renewmembershiponcontribution($params) {
+
+
+}
+
